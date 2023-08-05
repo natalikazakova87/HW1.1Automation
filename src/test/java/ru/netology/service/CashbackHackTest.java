590973bb2edf4b131bba5624d329f4b7b1842d7d
natalikazakova <natalikazakova87@gmail.com>
2023-08-05 14:50:10 +0700

    initial commit

--- src/test/java/ru/netology/service/CashbackHackTest.java
+++ src/test/java/ru/netology/service/CashbackHackTest.java
@@ -1,3 +1,36 @@
 package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CashbackHackTest {
+    @Test
+    public void testForLessAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testForMoreAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testForExactlyAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
 }
